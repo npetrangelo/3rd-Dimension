5ca2c687e6009105de173a5da524c7777876d06a
Nathan Petrangelo <nathan@petrangelo.org>
2017-01-16 17:05:23 -0500

    Adjusted Top View
    Updated parameters to match previous commit

--- src/Window.java
+++ src/Window.java
@@ -79,7 +79,7 @@ public class Window extends JFrame implements ActionListener
         Framework framework = new Framework(new CameraPerspective());
 //        Framework framework = new Framework(new CameraOrthographic());
 //        Framework framework = new Framework((CubePoint point) -> {
-//           return new Point((int) (500 * point.x/(point.y - 200)), (int)(500 * (point.z - 200)/(point.y - 200))); 
+//           return new Point((int) (500 * point.x/(point.y - 150)), (int)(500 * (point.z - 10)/(point.y - 150))); 
 //        });
         mainPanel.add(framework);
         mainPanel.add(UIPanel);
